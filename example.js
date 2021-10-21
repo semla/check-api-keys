b4e0bba1bd1ab40ef37137a3b375c961e4ab365f
Semla <semla.dev@gmail.com>
2021-10-21 09:17:15 +0200

    fix: importing package the right way

--- example.js
+++ example.js
@@ -5,7 +5,7 @@
 
 // See the README.md for an example how to use it in a build-script
 
-const checkAllKeys = require('./checkKeys')
+const checkKeys = require('check-api-keys')
 
 main()
 
@@ -26,5 +26,5 @@ optionsArr = [
     }
 ]
 
-  await checkAllKeys(optionsArr);
+  await checkKeys(optionsArr);
 }
